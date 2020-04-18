8d7e5694daa2c4a2140c230e61a2fca1c93025cb
priscillashort <pshort2@cocc.edu>
2020-04-18 13:37:47 -0700

    Initial Commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <title>Motorcycle Mania</title>
+  <link rel="stylesheet" type="text/css" media="all" href="/main.css">
+  <style>
+  </style>
+  <script src="/assets/javascript/main.js"></script>
+</head>
+<body>
+  <header>
+    <h1><a href="/">Motorcycle Mania!</a></h1>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/categories">Categories</a></li>
+        <li><a href="/manufacturers.html">Manufacturers</a></li>
+      </ul>
+      <a id="lnk_add" href="/new.php">+ Add a Bike</a>
+    </nav>
+  </header>
+  <main>
