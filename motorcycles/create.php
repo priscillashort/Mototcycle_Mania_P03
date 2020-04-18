f907e93f95532da77634c204222ee9ce7bc89488
priscillashort <pshort2@cocc.edu>
2020-04-18 16:25:34 -0700

    Capture form data and display on create page

--- motorcycles/create.php
+++ motorcycles/create.php
@@ -1,18 +1,68 @@
-<?php include '../header.php'; ?>
+<?php 
+	include '../header.php'; 
+	
+	$capture_category_id = $_POST['category_id'];
+	$capture_manufacturer_id = $_POST['manufacturer_id'];
+
+	switch ($capture_category_id) {
+		case "":
+			$category_id = "Select...";
+			break;
+		case 1:
+			$category_id = "Naked";
+			break;
+		case 2:
+			$category_id = "Dual-Sport";
+			break;
+		case 3:
+			$category_id = "Cruiser";
+			break;
+		case 4:
+			$category_id = "Sport";
+			break;
+	}
+
+	switch ($capture_manufacturer_id) {
+		case "":
+			$manufacturer_id = "Select...";
+			break;
+		case 1:
+			$manufacturer_id = "Indian";
+			break;
+		case 2:
+			$manufacturer_id = "Honda";
+			break;
+		case 3:
+			$manufacturer_id = "Triumph";
+			break;
+		case 4:
+			$manufacturer_id = "Victory";
+			break;
+		case 5:
+			$manufacturer_id = "BMW";
+			break;
+	}
+
+	$model = $_POST['model'];
+	$year = $_POST['year'];
+	$motor_cc = $_POST['motor_cc'];
+	$motor_hp = $_POST['motor_hp'];
+?>
+    <div>
     <h2>Motorcycle Saved</h2>
     <p>The new motorcycle has been added to the list.</p>
     <h3>Category:</h3>
-    <p>(category id from form)</p>
+    <p><?= $category_id ?></p>
     <h3>Manufacturer:</h3>
-    <p>(manufacturer id from form)</p>
+    <p><?= $manufacturer_id ?></p>
     <h3>Model:</h3>
-    <p>(model from form)</p>
+    <p><?= $model ?></p>
     <h3>Year:</h3>
-    <p>(year from form)</p>
+    <p><?= $year ?></p>
     <h3>Motor cc:</h3>
-    <p>(cc from form)</p>
+    <p><?= $motor_cc ?></p>
     <h3>Motor hp:</h3>
-    <p>(hp from form)</p>
+    <p><?= $motor_hp ?></p>
     <h3>Image:</h3>
     <p>(save for later / try doing it as a bonus challenge)</p>
     </div>
